4bd64a28d09efe7e79f00ccfd81daca05b235cb3
Raphael Schuhmann <raphael221@outlook.de>
2024-11-01 18:11:56 +0100

    Implemented working project file generation for c and c++

--- src/projectgen.cpp
+++ src/projectgen.cpp
@@ -46,18 +46,19 @@ void generateFolders(const std::string& projectName, const std::string& inputPro
 
 void generateFiles(std::string* prefs) {
   std::string dir = convertShortDir(prefs[1]);
-  std::cout << prefs[3];
 
   dir += prefs[0] + "/";
 
   if (prefs[4] == "n") {
-    generateFile("/home/raphael/Development/project-setup/templates/CMakeListsTemplate.txt", dir, "CMakeLists.txt");
+    generateFile(convertShortDir("~/Development/project-setup/templates/CMakeListsTemplate.txt"), dir, "CMakeLists.txt", prefs);
   }
 
-  if (prefs[3] == "c++") {
-    generateFile("/home/raphael/Development/project-setup/templates/mainCpp.txt", dir + "src/", "main.cpp");
-  } else if (prefs[3] == "c") {
-    generateFile("/home/raphael/Development/project-setup/templates/mainC.txt", dir + "src/", "main.c");
+  if (prefs[2] == "c++") {
+    generateFile(convertShortDir("~/Development/project-setup/templates/mainCpp.txt"), dir + "src/", "main.cpp");
+    generateFile(convertShortDir("~/Development/project-setup/templates/gitignoreCpp.txt"), dir, ".gitignore");
+  } else if (prefs[2] == "c") {
+    generateFile(convertShortDir("~/Development/project-setup/templates/mainC.txt"), dir + "src/", "main.c");
+    generateFile(convertShortDir("~/Development/project-setup/templates/gitignoreC.txt"), dir, ".gitignore");
   }
 }
 
@@ -82,14 +83,82 @@ std::string readTemplate(const std::string& path) {
   return buffer.str();
 }
 
-void generateFile(const std::string& templatePath, const std::string& projectPath, std::string file) {
-  std::ofstream File(projectPath + file);
-  if (!File) {
-    std::cerr << "Error generating file: " << file << std::endl;
+std::string replacePlaceholders(const std::string& templateStr, const std::string& projectName, const std::string& language, const std::string& version) {
+  std::string result = templateStr;
+
+  std::string lang;
+  if (language == "c++") {
+    lang = "CXX";
+  } else if (language == "c") {
+    lang = "C";
+  } else {
+    std::cerr << "Language is not supported: " << language << std::endl;
     exit(1);
   }
 
-  File << readTemplate(templatePath);
-  File.close();
-  return;
+  std::string fileEnding;
+  if (language == "c++") {
+    fileEnding = "cpp";
+  } else if (language == "c") {
+    fileEnding = language;
+  } else {
+    std::cerr << "Language is not supported: " << language << std::endl;
+    exit(1);
+  }
+  
+  size_t pos = 0;
+
+  while ((pos = result.find("{{PROJECT_NAME}}", pos)) != std::string::npos) {
+    result.replace(pos, std::string("{{PROJECT_NAME}}").length(), projectName);
+    pos += projectName.length();
+  }
+
+  pos = 0;
+  while ((pos = result.find("{{LANGUAGE}}", pos)) != std::string::npos) {
+    result.replace(pos, std::string("{{LANGUAGE}}").length(), lang);
+    pos += lang.length();
+  }
+
+  pos = 0;
+  while ((pos = result.find("{{LANGUAGE_VERSION}}", pos)) != std::string::npos) {
+    result.replace(pos, std::string("{{LANGUAGE_VERSION}}").length(), version);
+    pos += version.length();
+  }
+
+  pos = 0;
+  while ((pos = result.find("{{FILE}}", pos)) != std::string::npos) {
+    result.replace(pos, std::string("{{FILE}}").length(), fileEnding);
+    pos += fileEnding.length();
+  }
+
+  return result;
+}
+
+
+void generateFile(const std::string& templatePath, const std::string& projectPath, std::string file, std::string* prefs) {
+  if (prefs != nullptr) {
+    std::string templateContent = readTemplate(templatePath);
+    
+    std::string populatedTemplate = replacePlaceholders(templateContent, prefs[0], prefs[2], prefs[3]);
+
+    std::ofstream File(projectPath + file);
+    if (!File) {
+      std::cerr << "Error generating file: " << file << std::endl;
+      exit(1);
+    }
+
+    File << populatedTemplate;
+    File.close();
+    return;
+  } else {
+    std::ofstream File(projectPath + file);
+    if (!File) {
+      std::cerr << "Error generating file: " << file << std::endl;
+      exit(1);
+    }
+
+    File << readTemplate(templatePath);
+    File.close();
+    return;
+  }
 }
